d366b1808d347b56df920601036eef024f41db9c
T-R-a-c-k <117862440+VisforVirus@users.noreply.github.com>
2023-05-02 13:37:57 -0400

    router testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Button from "react-bootstrap/Button";
 import "./App.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
