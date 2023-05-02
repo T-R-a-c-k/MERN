95523f33d32e35d8c7147b7daea78ee5941610e0
T-R-a-c-k <tylerrackCA@gmail.com>
2023-05-02 16:01:52 -0400

    axios setup with react router to allow for communication with the backend server

--- client/src/pages/sign_up.jsx
+++ client/src/pages/sign_up.jsx
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+
 function SignUp() {
+  const DEFAULT_FORM_OBJECT = {
+    firstName: "",
+    lastName: "",
+  };
+
+  const [validated, setValidated] = useState(false);
+  const [form, setForm] = useState(DEFAULT_FORM_OBJECT);
+
+  const handleSubmit = async (event) => {
+    axios.post("https://localhost:8080/hello");
+
+    setForm(DEFAULT_FORM_OBJECT);
+
+    setValidated(true);
+    event.preventDefault();
+  };
+
+  const handleChange = (key) => (e) => {
+    setForm({
+      ...form,
+      [key]: e.currentTarget.value,
+    });
+  };
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <Container>
+      <h1>Enter your information</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="firstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={form.firstName}
+              onChange={handleChange("firstName")}
+            />
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="lastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={form.lastName}
+              onChange={handleChange("lastName")}
+            />
+          </Form.Group>
+        </Row>
+        <Button type="submit">Submit form</Button>
+      </Form>
+    </Container>
   );
 }
 
