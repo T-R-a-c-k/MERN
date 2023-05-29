27471b436e85371efcf5e307f83bc2a9abf5d99d
T-R-a-c-k <tylerrackCA@gmail.com>
2023-05-29 13:18:55 -0400

    Wrote first test

--- client/src/tests/About.test.js
+++ client/src/tests/About.test.js
@@ -1,9 +1,46 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import About from "../pages/About";
+import createAccordianInformation from "../formatting/accordianInformation";
 
-describe("About component", () => {
+const aboutAccordianInfo = [
+  [
+    createAccordianInformation(
+      "Common Question",
+      "A common answer that would be displayed for the stated common question or concern."
+    ),
+  ],
+  [
+    createAccordianInformation(
+      "Common Question",
+      "A common answer that would be displayed for the stated common question or concern."
+    ),
+    createAccordianInformation(
+      "Common Question",
+      "A common answer that would be displayed for the stated common question or concern."
+    ),
+  ],
+  [
+    createAccordianInformation(
+      "Common Question",
+      "A common answer that would be displayed for the stated common question or concern."
+    ),
+    createAccordianInformation(
+      "Common Question",
+      "A common answer that would be displayed for the stated common question or concern."
+    ),
+  ],
+];
+
+const aboutAccordianTitles = [
+  "An important Concern",
+  "Some Other Issue",
+  "Something to Consider",
+];
+
+describe("About Component", () => {
   it("renders without errors", () => {
     render(
       <MemoryRouter>
@@ -11,5 +48,28 @@ describe("About component", () => {
       </MemoryRouter>
     );
     // Add additional assertions if necessary
+  });
+  it("renders the contents correctly", () => {
+    render(
+      <MemoryRouter>
+        <About toDisplay={aboutAccordianInfo} titles={aboutAccordianTitles} />
+      </MemoryRouter>
+    );
+    // Assert the presence of the accordion titles
+
+    aboutAccordianTitles.forEach((title) => {
+      expect(screen.getByText(title)).toBeInTheDocument();
+    });
+
+    // Assert the presence of the accordion items
+    aboutAccordianInfo.flat().forEach((item) => {
+      const headerElements = screen.queryAllByText(item.header);
+      const descriptionElements = screen.queryAllByText(item.description);
+
+      expect(headerElements.length).toBeGreaterThan(0);
+      expect(descriptionElements.length).toBeGreaterThan(0);
+    });
+
+    // You can add more assertions based on your component's rendering behavior
   });
 });
