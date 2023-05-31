f100dfd4c138b3e6d5d57d7144fa097482e3193b
T-R-a-c-k <tylerrackCA@gmail.com>
2023-05-31 13:39:44 -0400

    Finished first update tests

--- client/src/pages/DepartmentUpdate.jsx
+++ client/src/pages/DepartmentUpdate.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import axios from "axios";
-import { Form, Button } from "react-bootstrap";
-import { requestHeaders } from "../server headers/headers";
+import { useParams, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
 import { useContext } from "react";
+import { requestHeaders } from "../server headers/headers";
 
 function DepartmentUpdate({ method }) {
   const DEFAULT_FORM_OBJECT = {
@@ -13,13 +13,12 @@ function DepartmentUpdate({ method }) {
     location: "",
     budget: 0,
   };
-  const { tokenInstance } = useContext(UserContext);
   const navigate = useNavigate();
-  const [validated, setValidated] = useState(false);
   const { id } = useParams();
+  const { tokenInstance } = useContext(UserContext);
   const [form, setForm] = useState(DEFAULT_FORM_OBJECT);
   const [err, setErr] = useState("");
-
+  const [validated, setValidated] = useState(false);
   useEffect(() => {
     const getData = async () => {
       const values = await axios.get(
@@ -31,7 +30,7 @@ function DepartmentUpdate({ method }) {
     if (id) {
       getData();
     }
-  }, [id, tokenInstance]);
+  }, [tokenInstance, id]);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -48,6 +47,7 @@ function DepartmentUpdate({ method }) {
       [key]: e.target.value,
     });
   };
+
   const submitHandler = async (e) => {
     const submitForm = e.currentTarget;
     e.preventDefault();
@@ -75,72 +75,73 @@ function DepartmentUpdate({ method }) {
   };
 
   return (
-    <Container
-      fluid
-      style={{
-        textAlign: "center",
-        width: "50%",
-        marginTop: "2%",
-        marginBottom: "2%",
-      }}
-    >
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={(e) => {
-          submitHandler(e);
+    <>
+      <Container
+        fluid
+        style={{
+          textAlign: "center",
+          width: "50%",
+          marginTop: "2%",
+          marginBottom: "2%",
         }}
       >
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Department Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder={"Name"}
-            value={form.name}
-            onChange={updateFormValue("name")}
-            required
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            A department is required.
-          </Form.Control.Feedback>
-        </Form.Group>
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Department Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder={"Name"}
+              value={form.name}
+              onChange={updateFormValue("name")}
+              required
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              A department is required.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicLocation">
-          <Form.Label>Location</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="location"
-            value={form.location}
-            onChange={updateFormValue("location")}
-            required
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            A location is required.
-          </Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicLocation">
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="location"
+              value={form.location}
+              onChange={updateFormValue("location")}
+              required
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              A location is required.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicBudget">
-          <Form.Label>Budget</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="0"
-            value={form.budget}
-            onChange={updateFormValue("budget")}
-            required
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            A budget is required.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+          <Form.Group className="mb-3" controlId="formBasicBudget">
+            <Form.Label>Budget</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="0"
+              value={form.budget}
+              onChange={updateFormValue("budget")}
+              required
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              A budget is required.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
   );
 }
-
 export default DepartmentUpdate;
---
+++ client/src/tests/DepartmentUpdate.test.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { render, waitFor, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import axios from "axios";
+import DepartmentUpdate from "../pages/DepartmentUpdate";
+import { UserContext } from "../context/UserProvider";
+import { MemoryRouter } from "react-router-dom";
+import Router from "react-router";
+
+jest.mock("axios"); // Mock the axios module
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useParams: jest.fn(),
+}));
+
+describe("update component", () => {
+  it("gets data from the api", async () => {
+    jest.spyOn(Router, "useParams").mockReturnValue({ id: "1" });
+    const mockedData = {
+      _id: "1",
+      name: "Department 1",
+      location: "Location 1",
+      budget: 1000,
+    };
+
+    await axios.get.mockResolvedValue({ data: mockedData }); // Mock the axios.get function
+
+    const tokenInstance = { token: "mockToken" };
+
+    render(
+      <MemoryRouter>
+        <UserContext.Provider value={{ tokenInstance }}>
+          <DepartmentUpdate />
+        </UserContext.Provider>
+      </MemoryRouter>
+    );
+
+    //Linter please stop yelling at me
+    await waitFor(() => {
+      const departmentName = screen.getByRole("textbox", {
+        name: /department name/i,
+      });
+      expect(departmentName.value).toBe(mockedData.name);
+    });
+    expect(axios.get).toHaveBeenCalledWith(
+      `http://localhost:8080/department/${mockedData._id}/update`,
+      { headers: { Authorization: `Bearer ${tokenInstance.token}` } }
+    );
+  });
+  it("should submit the form with updated values", async () => {
+    jest.spyOn(Router, "useParams").mockReturnValue({ id: "1" });
+    const mockedData = {
+      _id: "1",
+      name: "Department 1",
+      location: "Location 1",
+      budget: 1000,
+    };
+
+    await axios.get.mockResolvedValue({ data: mockedData });
+    const tokenInstance = { token: "mockToken" };
+
+    render(
+      <MemoryRouter>
+        <UserContext.Provider value={{ tokenInstance }}>
+          <DepartmentUpdate />
+        </UserContext.Provider>
+      </MemoryRouter>
+    );
+
+    const departmentNameInput = await screen.findByLabelText("Department Name");
+    const locationInput = await screen.findByLabelText("Location");
+    const budgetInput = await screen.findByLabelText("Budget");
+    const submitButton = await screen.findByRole("button", {
+      name: /submit/i,
+    });
+
+    const mockedNewData = {
+      name: "New Department",
+      location: "New Location",
+      budget: "2000",
+    };
+
+    fireEvent.change(departmentNameInput, {
+      target: { value: mockedNewData.name },
+    });
+    fireEvent.change(locationInput, {
+      target: { value: mockedNewData.location },
+    });
+    fireEvent.change(budgetInput, { target: { value: mockedNewData.budget } });
+
+    expect(departmentNameInput.value).toBe(mockedNewData.name);
+    expect(locationInput.value).toBe(mockedNewData.location);
+    expect(budgetInput.value).toBe(mockedNewData.budget);
+
+    fireEvent.click(submitButton);
+    axios.put.mockReturnValue(mockedNewData);
+  });
+});
